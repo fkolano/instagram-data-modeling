6f605c05df5f9787d1ffd1d58ae8a3490dd4b07c
fkolano <fkolano@gmail.com>
2022-08-04 13:45:15 +0000

    final commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,45 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Media(Base):
+    __tablename__ = 'media'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post_id = Column(Integer, ForeignKey('post.id'), primary_key=True)
+    media_type = Column(String(250), nullable=False, )
+    url = Column(String(250), nullable=False, unique=True)
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(256), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'), primary_key=True, nullable=False, )
+    
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    
+    ##def to_dict(self):
+        ##return {}
 
 ## Draw from SQLAlchemy base
 try:
